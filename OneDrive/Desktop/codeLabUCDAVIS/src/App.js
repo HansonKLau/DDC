9a0bde8d764357f0feae30cb906bb2adecbfc0ae
Jason Nguyen <jasonnguyenphhs@gmail.com>
2022-04-16 13:22:44 -0700

    test

---
+++ OneDrive/Desktop/codeLabUCDAVIS/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Main, Navbar } from "./components/BasicElements";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
