d5765f64e78712878a9a8b86763166eb2709e423
Jason Nguyen <jasonnguyenphhs@gmail.com>
2022-04-16 13:48:09 -0700

    skeleton of front page

---
+++ davisdc/src/frontpage.js
@@ -0,0 +1,60 @@
+import "./frontpage.css"
+import { Button, Form, Container, Row, Col} from 'react-bootstrap';
+import React, {useState, useEffect} from "react";
+
+
+function frontPage(){
+    return (
+    <Container className = "logo">
+    
+        <div className = "front">
+            <Row>
+                <Col xs = {10}>
+            <div className = "DInDavis">
+                D
+                <div className = "AvisInDavis">
+                    avis
+                </div>
+            </div>
+                </Col>
+            </Row>
+            <Row>
+            <Col xs = {11}>
+            <div className = "DInDining">
+                D
+                <div className = "IningInDining">
+                    ining
+                </div>
+            </div>
+            </Col>
+            </Row>
+            <Row>
+            <Col xs = {14}>
+            <div className = "CInCommons">
+                C
+                <div className = "OmmonsInCommon">
+                    ommons
+                </div>
+            </div>
+            </Col>
+            </Row>
+        </div>  
+        <div className = "side">
+            <div className = "Tecero">
+                Tecero
+            </div>
+            <div className = "Latitude">
+                Latitude
+            </div>
+            <div className = "Segundo">
+                Segundo
+            </div>
+            <div className = "Cuarto">
+                Cuarto
+            </div>
+        </div>
+        </Container>
+)
+}
+
+export default frontPage;
