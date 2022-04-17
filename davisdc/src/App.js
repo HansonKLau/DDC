8c264a172b38aa0c6fb036d22f6e5a1e7fd94620
Jason Nguyen <jasonnguyenphhs@gmail.com>
2022-04-16 17:26:13 -0700

    added dropdown

--- davisdc/src/App.js
+++ davisdc/src/App.js
@@ -1,11 +1,12 @@
 import FrontPage from './frontpage.js'
+import SecondPage from './secondpage.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <FrontPage />
+      <SecondPage />
     </div>
   );
 }
